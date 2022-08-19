c504824804d7765f05d69ffe1e16e7947acd518f
royyan005 <royyan005@gmail.com>
2022-08-20 00:25:41 +0700

    change database

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('app-ppi','root','',{
-    host: "localhost",
-    dialect: "mysql"
+const db = new Sequelize('dcgea3asbfrt0f','zzilbkpfpvhrbl','ad4b18dbf114b844be45a6e3fabcb9ff7f18f80e81ff1107ddf71e19fb6d31c1',{
+    host: "ec2-3-208-79-113.compute-1.amazonaws.com",
+    dialect: "postgresql"
 })
 
 export default db;
