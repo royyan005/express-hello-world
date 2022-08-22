8dceda98481b1f90e87012d312003922760e46ff
royyan005 <royyan005@gmail.com>
2022-08-22 18:04:57 +0700

    change database to local

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('dcgea3asbfrt0f','zzilbkpfpvhrbl','ad4b18dbf114b844be45a6e3fabcb9ff7f18f80e81ff1107ddf71e19fb6d31c1',{
-    host: "ec2-3-208-79-113.compute-1.amazonaws.com",
+const db = new Sequelize('app-ppi','postgres','123123',{
+    host: "localhost",
     dialect: "postgres",
     dialectOptions: {
         ssl: {
